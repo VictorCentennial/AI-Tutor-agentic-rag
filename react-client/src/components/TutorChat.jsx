fa368d6049a7140eb3a995d9e0898066d8b61175
namneet8 <147285108+namneet8@users.noreply.github.com>
2025-01-25 18:56:16 -0500

    Update TutorChat.jsx

--- react-client/src/components/TutorChat.jsx
+++ react-client/src/components/TutorChat.jsx
@@ -255,41 +255,6 @@ main
             )}
           </div>
         </div>
-
-  return (
-    <Container fluid className="mt-4 relative w-full">
-      {isTutoringStarted && (
-        <div className="absolute right-0 top-0 px-4 py-2 rounded-md text-right">
-          <span>🕒 Time Left: </span>
-          <span
-            className={`${remainingTime <= 300 && remainingTime !== 0 ? "blinking-red" : ""}`}
-          >
-            {formatTime(remainingTime)}
-          </span>
-        </div>
-      )}
-
-
-      {/* Pop-up Modal */}
-      <Modal show={showWarning} onHide={() => setShowWarning(false)} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Session Ending Soon</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          Only 5 minutes are left in your session. Please wrap up your work.
-        </Modal.Body>
-        <Modal.Footer>
-          <button
-            className="btn btn-primary"
-            onClick={() => setShowWarning(false)}
-          >
-            OK
-          </button>
-        </Modal.Footer>
-      </Modal>
-
-      {!isTutoringStarted ? (
-        <TutorStart onStartTutoring={handleStartTutoring} isLoading={isLoading} />
       ) : (
         <>
           {isTutoringStarted && (
