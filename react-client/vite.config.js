361199f6df8ffb10dfafc53c8d3c1b7680d99265
vic70 <victortse70@gmail.com>
2025-03-17 01:39:29 -0400

    modify vite file to env variable

--- react-client/vite.config.js
+++ react-client/vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, '.', '')
 
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+  return {
+    plugins: [react()],
+
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://127.0.0.1:5001',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
