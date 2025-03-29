f6eb123d71a34e3def42ce527a90822e0275a315
vic70 <victortse70@gmail.com>
2025-03-28 22:22:08 -0400

    modify vite to use config.js

--- react-client/vite.config.js
+++ react-client/vite.config.js
@@ -1,23 +1,25 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { getServerConfig } from './src/utils/config'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, '.', '')
+  //const env = loadEnv(mode, '.', '')
+  const config = getServerConfig()
 
   return {
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_URL || 'http://127.0.0.1:5001',
+          target: config.API_URL || 'http://127.0.0.1:5001',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
         '/serve-file': {
-          target: env.VITE_API_URL || 'http://127.0.0.1:5001', // Ensure it points to Flask
+          target: config.API_URL || 'http://127.0.0.1:5001', // Ensure it points to Flask
           changeOrigin: true,
         }
       }
